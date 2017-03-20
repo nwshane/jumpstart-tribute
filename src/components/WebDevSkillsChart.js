5fdc2c20c68f1f508cb8b15c55a1ceeafa95106d
nwshane <django09@gmail.com>
2017-03-20 23:28:30 +0400

    Implemented first version of web dev skills chart.

--- src/components/WebDevSkillsChart.js
+++ src/components/WebDevSkillsChart.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
+import { Chart } from 'react-google-charts';
+
+const columns = [
+  {
+    type: 'string',
+    label: ''
+  },
+  {
+    type: 'string',
+    label: 'Dev Tool'
+  },
+  {
+    type: 'date',
+    label: 'Start Date'
+  },
+  {
+    type: 'date',
+    label: 'End Date'
+  },
+]
+
+const endDate = new Date()
+
+const rows = [
+  [ '', 'React', new Date(2016, 9, 1), endDate ],
+  [ '', 'JavaScript', new Date(2016, 1, 1), endDate ],
+  [ '', 'CSS', new Date(2015, 7, 1), endDate ],
+  [ '', 'Rails', new Date(2014, 8, 1), endDate ]
+]
 
 class WebDevSkillsChart extends Component {
   render () {
     return (
-      <div className='web-dev-skills-chart'>REPLACE: timeline chart</div>
+      <Chart
+        chartPackages={['corechart', 'timeline']}
+        chartType="Timeline"
+        columns={columns}
+        rows={rows}
+        options={{timeline: { groupByRowLabel: false, showRowLabels: false }}}
+        graph_id="web-dev-skills-chart"
+        width="100%"
+        height="250px"
+      />
     )
   }
 }
